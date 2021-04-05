e6a751d689797862fee075a2f85c51578920767d
unknown <joseparanan@gmail.com>
2021-04-05 18:30:55 -0300

    #

--- spring-e-commerce/armazemNatural/armazemNatural/src/main/java/com/cultivar/armazemNatural/repository/ProdutoRepository.java
+++ spring-e-commerce/armazemNatural/armazemNatural/src/main/java/com/cultivar/armazemNatural/repository/ProdutoRepository.java
@@ -1,5 +1,13 @@
 package com.cultivar.armazemNatural.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cultivar.armazemNatural.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
 }
